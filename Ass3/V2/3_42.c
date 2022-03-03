f9dc3c8f77b0018acb28d402e40c87ff7876e5fc
CommitSudoku <97761927+isaackhlam@users.noreply.github.com>
2022-03-03 23:23:52 +0800

    Update 3_42.c

--- Ass3/V2/3_42.c
+++ Ass3/V2/3_42.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define lchild(i) 2*i
-#define rchild(i) 2*i+1
+#include <string.h>
+#define lchild(i) 2*i+1
+#define rchild(i) 2*i+2
+#define NUM 1024
 int painter = 0;
+int input[NUM];
 
 typedef struct node{
     int data;
@@ -12,56 +15,85 @@ typedef struct node{
     struct node* parent;
 }NODE;
 
-void build(NODE[],int);
 void paint(NODE*);
 void check(NODE*);
+void release(NODE*);
+void reset(NODE*);
+NODE* build(void);
+NODE* create(NODE*, int);
+NODE* insert(NODE*, NODE*, int, int);
 
 int main(void){
-    int test,n;
-    scanf("%d",&test);
-    while(test--){
-        scanf("%d",&n);
-        NODE tree[n+1];
-        build(tree,n);
-        check(tree+1);
+    int n;
+    scanf("%d",&n);
+    while(n--){
+        NODE* root = build();
+        check(root);
+        if(!root->painted) painter++;
         printf("%d\n", painter);
-        painter=0;
+        reset(root);
     }
     return 0;
 }
 
-void build(NODE tree[],int n){
-    for(int i=1; i<n+1; i++){
-        scanf("%d",&tree[i].data);
-        tree[i].painted = 0; 
-    }
-        tree[1].parent = NULL;
-    for(int i=1; i<n+1; i++){
-        if(lchild(i)<n+1&&tree[i].data){
-            tree[i].lchild = &tree[lchild(i)];
-            tree[i].lchild->parent = &tree[i];
-        }else
-            tree[i].lchild = NULL;
-        if(rchild(i)<n+1&&tree[i].data&&tree[rchild(i)].data){
-            tree[i].rchild = &tree[rchild(i)];
-            tree[i].rchild->parent = &tree[i];
-        }else
-            tree[i].rchild = NULL;   
-    }
+NODE* build(void){
+    int n;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+        scanf("%d",&input[i]);
+    NODE* root = insert(NULL, root, 0, n);
+    return root;
+}
+
+NODE* create(NODE* p, int data){
+	NODE* tmp = (NODE*)malloc(sizeof(NODE));
+	tmp->lchild = tmp->rchild = NULL;
+	tmp->parent = p;
+	tmp->data = data;
+	tmp->painted = 0;
+	return tmp;
+}
+
+NODE* insert(NODE* parent, NODE* root, int i, int n){
+	if(!input[i]) return NULL;
+	if(i<n){
+		root = create(parent, input[i]);
+		root->parent = parent;
+		root->lchild = insert(root, root->lchild,lchild(i),n);
+		root->rchild = insert(root, root->rchild,rchild(i),n);
+	}
+	return root;
 }
 
 void check(NODE* p){
-    if(p){
-        check(p->lchild);
-        check(p->rchild);
-        if(!p->painted) paint(p->parent);
-    }
+	if(p->lchild)
+		check(p->lchild);
+	if(p->rchild)
+		check(p->rchild);
+	if(p->lchild && !(p->lchild->painted))
+        paint(p);
+    else if(p->rchild && !(p->rchild->painted))
+        paint(p);
 }
 
 void paint(NODE* p){
+	painter++;
     p->painted = 1;
     if(p->parent) p->parent->painted = 1;
     if(p->lchild) p->lchild->painted = 1;
     if(p->rchild) p->rchild->painted = 1;
-    painter++;
+}
+
+void release(NODE* p){
+	if(p){
+		release(p->lchild);
+		release(p->rchild);
+		free(p);
+	}
+}
+
+void reset(NODE* p){
+    painter = 0;
+    release(p);
+    memset(input,'0',sizeof(input));
 }
