a9872af3aac04903c195cfaf6d61ef40ef656909
CommitSudoku <97761927+isaackhlam@users.noreply.github.com>
2022-05-12 02:06:14 +0800

    Update 5_21.c

--- Ass4/5_21.c
+++ Ass4/5_21.c
@@ -1,22 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int* build(int);
+typedef struct Hashstruct{
+    int* key;
+    int* state;
+}Hash;
+
+Hash* build(int);
 int hashFunction(int, int);
 int hashNext(int, int, int);
-void insert(int*, int, int);
-int search(int*, int, int);
+void insert(Hash*, int, int);
+int search(Hash*, int, int);
 void solve(void);
-int count(int*, int, int);
+int count(Hash*, int, int);
 
 int main(void){
     solve();
     return 0;
 }
 
-int* build(int size){
-    int* h = (int*)malloc(sizeof(int) * size);
-    for(int i=0;i<size;i++) h[i] = 0;
+Hash* build(int size){
+    Hash* h = (Hash*)malloc(sizeof(Hash));
+    h->key = (int*)malloc(sizeof(int) * size);
+    h->state = (int*)malloc(sizeof(int) * size);
+    for(int i=0;i<size;i++) h->state[i] = 0;
     return h;
 }
 
@@ -28,20 +35,21 @@ int hashNext(int k, int data, int size){
     return (hashFunction(data, size) + k) % size;
 }
 
-void insert(int* h, int data, int size){
+void insert(Hash* h, int data, int size){
     int hashValue = hashFunction(data, size);
     int i = hashValue;
     int k = 1;
-    while(h[i])
+    while(h->state[i])
         i = hashNext(k++, data, size);
-    h[i] = data;
+    h->key[i] = data;
+    h->state[i] = 1;
 }
 
-int search(int* h, int data, int size){
+int search(Hash* h, int data, int size){
     int hashValue = hashFunction(data, size);
     int i = hashValue;
     int k = 1;
-    while(h[i] != data){
+    while(h->key[i] != data){
         i = hashNext(k++, data, size);
         if(k > size) return 0;
     }
@@ -54,7 +62,7 @@ void solve(void){
     while(n--){
         int size, product, data;
         scanf("%d %d",&size, &product);
-        int* h = build(size);
+        Hash* h = build(size);
         for(int i = 0; i<size; i++){
             scanf("%d",&data);
             insert(h, data, size);
@@ -64,18 +72,13 @@ void solve(void){
     }
 }
 
-int count(int* h, int product, int size){
+int count(Hash* h, int product, int size){
     int COUNT = 0;
-    if(!product){
-        for(int i=0; i<size; i++)
-            if(!h[i]) return (size-1)*(size-2)/2;
-        return 0;
-    } 
     for(int i=0; i<size-1; i++)
         for(int j=i+1; j<size; j++)
-            if((h[i]*h[j] <= product) && (h[i]*h[j]) && !(product % h[i]*h[j])){
-                int k = product / (h[i]*h[j]);
-                if(k >= h[i] && k >= h[j] && search(h, k, size))
+            if((h->key[i]*h->key[j] <= product) && (h->key[i]*h->key[j]) && !(product % h->key[i]*h->key[j]) ){
+                int k = product / (h->key[i]*h->key[j]);
+                if(k >= h->key[i] && k >= h->key[j] && search(h, k, size))
                     COUNT++;
             }
     return COUNT;
